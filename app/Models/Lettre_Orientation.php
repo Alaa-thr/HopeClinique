1252f6aefd2fded12473fcb8d57f4b675cb31226
SaiNour <saidinourelhouda61@gmail.com>
2021-01-01 15:46:54 +0100

    ajouter les models

---
+++ app/Models/Lettre_Orientation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lettre_Orientation extends Model
+{
+    use HasFactory;
+}
