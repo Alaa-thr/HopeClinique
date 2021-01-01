f6731f3a65c4b078725896158958a1f62ab58b71
SaiNour <saidinourelhouda61@gmail.com>
2021-01-01 16:59:22 +0100

    modify modele

--- app/Models/Prescription.php
+++ app/Models/Prescription.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Prescription extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'id','medecin_id','patient_id','date','nom_medecin','prenom_medecin'  ];
+
+      public function patient()
+        {
+            return $this->belongsTo('App\Models\Patient');
+        }
+        public function medecin()
+          {
+              return $this->belongsTo('App\Models\Medecin');
+          }
 }
--- app/Models/Secretaire.php
+++ app/Models/Secretaire.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Secretaire extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'nom','prenom','user_id','id'];
+      
 }
