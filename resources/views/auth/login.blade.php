d37e466ba71d58729887ffa05b6aabf5c2b07d0b
SaiNour <saidinourelhouda61@gmail.com>
2021-01-02 13:05:14 +0100

    modifier page login et reset password et affichage de doctor secretaire et admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card" style="margin-bottom: 110px;margin-top: 100px; ">
+        <div class="col-md-8 offset-md-3">
+            <div class="card" style="margin-bottom: 110px;margin-top: 100px;width:475px;">
 
 
                 <div class="card-body" >
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                        <div class="account-logo" style="margin-bottom: 40px;margin-top: 20px;margin-left:300px;">
+                        <div class="account-logo" style="margin-bottom: 40px;margin-top: 15px;margin-left:150px;">
                                 <a href="#"><img  src="{{ asset('visitorPage/img/logo.png')}}" alt=""></a>
                         </div>
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md- col-form-label text-md-right">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
